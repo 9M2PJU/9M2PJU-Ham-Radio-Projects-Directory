a7d2e964436e475b09c3d20cdcf2e6a6999448df
9M2PJU <9m2pju@hamradio.my>
2026-01-25 23:58:15 +0800

    Feature: Add PWA Install Support with Session-based Prompt

---
+++ docs/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = '9m2pju-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './favicon.png',
+    './brand-logo-final.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
